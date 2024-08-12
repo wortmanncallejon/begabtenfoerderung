8a9ac7dcce641038d5f29b8bda0ed9fc1e1201f3
Felix Wortmann Callejon <felix@wortmanncallejon.de>
2024-08-12 09:03:28 +0200

    Add migrationshintergrund script

--- visualise_data.R
+++ visualise_data.R
@@ -50,6 +50,20 @@ ggsave(here("_plots", "förderungsart.png"), width = 16, height = 9, dpi = 300, 
 data_agg |>
     filter(Variable == "Migrationshintergrund") |>
     mutate(Ausprägung = ifelse(Ausprägung == "Migrationshintergrund", "Ja", "Nein")) |>
+    ggplot(aes(x = Jahr, y = n, color = Ausprägung)) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(NULL, breaks = 2013:2022, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_y_continuous("Wachstum seit 2013", labels = scales::label_comma(1)) +
+    scale_color_viridis("Migrationshintergrund", discrete = T) +
+    theme_light(base_size = 14) +
+    theme(legend.position = "bottom")
+
+ggsave(here("_plots", "migrationshintergrund.png"), width = 16, height = 9, dpi = 300, units = "cm")
+
+data_agg |>
+    filter(Variable == "Migrationshintergrund") |>
+    mutate(Ausprägung = ifelse(Ausprägung == "Migrationshintergrund", "Ja", "Nein")) |>
     mutate(baseline = mean(ifelse(Jahr == 2013, n, NA), na.rm = T),
             .by = Ausprägung) |>
     mutate(indicator = (n/baseline)) |>
