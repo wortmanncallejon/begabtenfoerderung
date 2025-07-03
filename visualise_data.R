3ed87f11bb19c47730beacbbfad48fdb0751613b
FWC <felix@wortmanncallejon.de>
2025-07-03 12:37:58 +0200

    Add indicator script

--- visualise_data.R
+++ visualise_data.R
@@ -25,7 +25,7 @@ data_agg |>
     ggplot(aes(x = Jahr, y = N)) +
     geom_line() +
     geom_point() +
-    scale_x_continuous(NULL, breaks = 2013:2022, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_x_continuous(NULL, breaks = 2013:2024, label = function (x) paste0("'", substr(as.character(x),3,4))) +
     scale_y_continuous("Anzahl Geförderte", labels = scales::label_comma()) +
     theme_light(base_size = 14)
 
@@ -39,7 +39,7 @@ data_agg |>
     ggplot(aes(x = Jahr, y = n, color = BAFöG)) +
     geom_line() +
     geom_point() +
-    scale_x_continuous(NULL, breaks = 2013:2022, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_x_continuous(NULL, breaks = 2013:2024, label = function (x) paste0("'", substr(as.character(x),3,4))) +
     scale_y_continuous("Anzahl Geförderte", labels = scales::label_comma()) +
     scale_color_viridis("BAFöG-Berechtigung", discrete = T) +
     theme_light(base_size = 14) +
@@ -48,12 +48,31 @@ data_agg |>
 ggsave(here("_plots", "förderungsart.png"), width = 16, height = 9, dpi = 300, units = "cm")
 
 data_agg |>
+    filter(Variable == "Förderungsart") |>
+    mutate(BAFöG = ifelse(Ausprägung == "SKP", "Nein", "Ja")) |>
+    summarise(across(c(n, N), \(x) sum(x, na.rm = T)),
+              .by = c("Jahr", "BAFöG")) |>
+    mutate(baseline = mean(ifelse(Jahr == 2013, n, NA), na.rm = T),
+            .by = BAFöG) |>
+    mutate(indicator = (n/baseline)) |>
+    ggplot(aes(x = Jahr, y = indicator, color = BAFöG)) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(NULL, breaks = 2013:2024, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_y_continuous("Wachstum Geförderte", labels = scales::label_percent()) +
+    scale_color_viridis("BAFöG-Berechtigung", discrete = T) +
+    theme_light(base_size = 14) +
+    theme(legend.position = "bottom")
+
+ggsave(here("_plots", "förderungsart_indicator.png"), width = 16, height = 9, dpi = 300, units = "cm")
+
+data_agg |>
     filter(Variable == "Migrationshintergrund") |>
     mutate(Ausprägung = ifelse(Ausprägung == "Migrationshintergrund", "Ja", "Nein")) |>
     ggplot(aes(x = Jahr, y = n, color = Ausprägung)) +
     geom_line() +
     geom_point() +
-    scale_x_continuous(NULL, breaks = 2013:2022, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_x_continuous(NULL, breaks = 2013:2024, label = function (x) paste0("'", substr(as.character(x),3,4))) +
     scale_y_continuous("Wachstum seit 2013", labels = scales::label_comma(1)) +
     scale_color_viridis("Migrationshintergrund", discrete = T) +
     theme_light(base_size = 14) +
@@ -70,7 +89,7 @@ data_agg |>
     ggplot(aes(x = Jahr, y = indicator, color = Ausprägung)) +
     geom_line() +
     geom_point() +
-    scale_x_continuous(NULL, breaks = 2013:2022, label = function (x) paste0("'", substr(as.character(x),3,4))) +
+    scale_x_continuous(NULL, breaks = 2013:2024, label = function (x) paste0("'", substr(as.character(x),3,4))) +
     scale_y_continuous("Wachstum seit 2013", labels = scales::label_percent(1)) +
     scale_color_viridis("Migrationshintergrund", discrete = T) +
     theme_light(base_size = 14) +
